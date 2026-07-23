2075167c57ea2c2a74e9dd56ce990f35e913f7dd
Kamal Dhakad <kamaldkd09@gmail.com>
2026-07-23 16:51:25 +0530

    Time: 0 ms (100%), Space: 43.7 MB (69.84%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDiff(TreeNode root, int min, int max) {
+        if(root == null) return max-min;
+
+        min = Math.min(root.val, min);
+        max = Math.max(root.val, max);
+        int leftDiff = maxDiff(root.left, min, max);
+        int rightDiff = maxDiff(root.right, min, max);
+
+        return Math.max(leftDiff, rightDiff);
+    }
+    public int maxAncestorDiff(TreeNode root) {
+        return maxDiff(root, root.val, root.val);
+    }
+}
