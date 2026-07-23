f892e764a9b5188cf79854203fb41cce483ddb39
Kamal Dhakad <kamaldkd09@gmail.com>
2026-07-23 11:48:41 +0530

    Time: 22 ms (98.96%), Space: 54.3 MB (75.29%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,5 +1,5 @@
 class StockSpanner {
-    Deque<int[]> st;
+    Deque<int[]> st; // (price, span)
     int currentIndex;
 
     public StockSpanner() {
@@ -18,6 +18,59 @@ class StockSpanner {
         return span;
     }
 }
+
+// expolanation of why we can also storr span.
+
+// Instead of storing indices, store a pair:
+
+// (price, span)
+
+// For example:
+
+// (100,1)
+// (80,1)
+// (60,1)
+
+// Now suppose the next price is 70.
+
+// We pop:
+
+// (60,1)
+
+// and instead of just removing it, we add its span.
+
+// span = 1 + 1 = 2
+
+// Then push:
+
+// (70,2)
+
+// Now suppose the next price is 75.
+
+// Stack:
+
+// (100,1)
+// (80,1)
+// (70,2)
+
+// Pop:
+
+// (70,2)
+// span = 1 + 2 = 3
+
+// Pop:
+
+// (80,1)
+// span = 3 + 1 = 4
+
+// Push:
+
+// (75,4)
+
+// See the beauty?
+
+// We don't need to count days one by one. Every popped element already knows the span of the days it represents.
+
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
